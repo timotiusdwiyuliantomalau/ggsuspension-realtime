22db9da341ca118df871aba64b5a0f50f6edfb8a
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-04-19 13:28:25 +0700

    Update

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000", // Alamat frontend Anda
     methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
     credentials: true,
   },
 });
